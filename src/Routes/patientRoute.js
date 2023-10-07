dd734e0c04680ed446732c900a70fc2f688a4122
HabibaMohamedd4 <120689937+HabibaMohamedd4@users.noreply.github.com>
2023-10-08 01:24:13 +0300

    presriptions

--- src/Routes/patientRoute.js
+++ src/Routes/patientRoute.js
@@ -3,6 +3,7 @@ import Doctor from '../Models/doctorModel.js';
 import patient from '../Models/patientsModel.js';
 import appointmentModel from '../Models/appointmentModel.js';
 import doctorModel from '../Models/doctorModel.js';
+import prescriptions from '../Models/prescriptionsModel.js';
 
 
 const router = express.Router()
@@ -16,6 +17,16 @@ router.get('/getAppointment/:date', async (req, res) => {
     res.status(200).json(getAppointments)
 });
 
+// requirement number 54
+router.post('/:id', async (req, res) => {
+    const prescriptions = await patient.findById(req.params.id)
+    if (!prescriptions)
+        res.status(400).json({ message: "no presriptions found",success:false})
+    else {
+
+        res.status(200).json({Result:prescriptions, success:true})
+    }
+})
 // requirement number 38
 router.get('/getDoctor/:name', async (req, res) => {
     const getDoctors = await Doctor.find({ Name: new RegExp(`${req.params.name}`) });
@@ -36,4 +47,18 @@ router.get('/getDoctors', async(req, res)=>{
 router.get('/filterDoctors', async(req, res)=>{
 
 })
+
+router.put('/:id', async (req, res) => {
+    const prescriptions= await prescriptions.findById(req.params.id)
+    if (!doctor)
+        res.status(400).json({ message: "Doctor not found",success:false})
+    else {
+        const updatedDoctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        res.status(200).json({Result:updatedDoctor,success:true})
+    }
+})
+
+
+
 export default router;
