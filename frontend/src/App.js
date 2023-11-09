0fb664a5f7b4701ccecdcb2f9fbe2da4136bd718
Seif Hossam <seif.hossam26092002@gamil.com>
2023-11-09 21:47:39 +0200

    Add sign out button to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,35 @@ import RegisterDoctor from "./Pages/RegisterDoctor";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./components/login";
 import ForgotPassword from "./Pages/ForgotPassword";
-
+import Button from 'react-bootstrap/Button';
 
 function App() {
+
+  const signoutButtonFunc = () => {
+    sessionStorage.removeItem('token');
+    window.location.href = '/';
+  }
+
+
+
   return (
     <div className="App">
-      <Router basename="/Health-Plus">
-        <Routes>
-       
-          <Route path="/registerPatient" element={<RegisterPatient />} />
-          <Route path="/registerDoctor" element={<RegisterDoctor/>} />
-          <Route path="/forgotPassword" element={<ForgotPassword/>} />
+      {
+        window.location.pathname == '/Health-Plus/' || window.location.pathname == '/Health-Plus/registerPatient' || window.location.pathname == '/Health-Plus/registerDoctor'?
+          <></>
+          : <div className="signoutButton">
+            <Button variant="danger" onClick={signoutButtonFunc}>Sign Out</Button>
+          </div>
+      }
 
+      <Router basename="/Health-Plus">
+
+        <Routes>
+
+          <Route path="/registerPatient" element={<RegisterPatient />} />
+          <Route path="/registerDoctor" element={<RegisterDoctor />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </div>
