4fac27c21e6ff60d14947414f8bed615d62de215
Seif Hossam <seif.hossam26092002@gamil.com>
2023-11-27 02:43:30 +0200

    Add lodash package and update user profile endpoint

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,9 +1,9 @@
 import Avatar from "./Avatar.jsx";
 
-export default function Contact({id,username,onClick,selected,online}) {
+export default function Contact({ id, username, onClick, selected, online }) {
   return (
     <div key={id} onClick={() => onClick(id)}
-         className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(selected ? 'bg-blue-50' : '')}>
+      className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer " + (selected ? 'bg-blue-50' : '')}>
       {selected && (
         <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
       )}
