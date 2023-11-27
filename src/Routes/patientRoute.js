a0b5c4969b60931e925481f21d58238f0fb2152d
HabibaMohamedd4 <120689937+HabibaMohamedd4@users.noreply.github.com>
2023-11-27 05:49:09 +0200

    reschedule an appointment untested

--- src/Routes/patientRoute.js
+++ src/Routes/patientRoute.js
@@ -451,6 +451,37 @@ router.post('/bookAppointment', protect, async (req, res) => {
 
 
 })
+
+//reschedule an appointment req.47
+
+
+    router.put('/rescheduleAppointment/:id', protect, async (req, res) => {
+        const exists = await patientModel.findOne(req.user);
+        if (!exists) {
+            return res.status(400).json({ message: "Patient not found", success: false })
+        }
+    
+        const appId = req.params.id;
+        const newdate= req.body.Date ;
+        const appointment= await appointments.findById(appId);
+        //add ba3d manzt el available slots
+        //const dId = appointment.DoctorId ;
+       // const availslot = await docAvailableSlots.findOne({ DoctorId: dId, Date: newdate });
+       // if (!availslot){
+       //     return (res.status(400).send({ error: "This slot is not available", success: false }));
+      //  }
+console.log(appId);
+        result = await appointment.Update( { Date : newdate},
+            {Status :"rescheduled"});
+     
+
+       // await docAvailableSlots.deleteOne({ DoctorId: dId, Date: newdate });
+        return res.status(200).json({ Result: result, success: true });
+    }
+
+)
+
+
 router.get('/bookAppointmentCard/:pid/:did/:date/:famId/:fees/:fam', async (req, res) => {
     const pId = req.params.pid;
     const dId = req.params.did;
