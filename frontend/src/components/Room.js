886c88da666906e0ded140e2a486800c569a3fc7
Seif Hossam <seif.hossam26092002@gamil.com>
2023-12-03 21:50:52 +0200

    Fix socket connection and call functionality

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -8,6 +8,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate, createSearchParams } from "react-router-dom";
 import { faVideo, faPhoneSlash, faMicrophone, faVideoSlash, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons'
 
 // const socket = io('http://localhost:5000')
@@ -22,7 +23,7 @@ socket.on('connect', () => {
 });
 console.log('socket', socket);
 function Room() {
-    const [me, setMe] = useState("")
+    // const [me, setMe] = useState("")
     const [stream, setStream] = useState()
     const [receivingCall, setReceivingCall] = useState(false)
     const [caller, setCaller] = useState("")
@@ -38,67 +39,139 @@ function Room() {
     const connectionRef = useRef()
 
     const location = useLocation();
+    const navigate = useNavigate();
     const searchParams = new URLSearchParams(location.search);
 
     // Get specific parameters
-    const videoParam = Boolean(searchParams.get('video')) == true;
+    // const videoParam = Boolean(searchParams.get('video')) == 'true';
+    const videoParam = location.state.video;
 
-    const recipientParam = searchParams.get('recipient');
+    // const recipientParam = searchParams.get('recipient');
+    const recipientParam = location.state.recipient;
 
-    const isAnswerCall = searchParams.get('answerCall') == 'true';
+    // const isAnswerCall = searchParams.get('answerCall') == 'true';
+    const isAnswerCall = location.state.answerCall;
+    // const me = searchParams.get('me');
+    const me = location.state.me;
+    // const callerUser = searchParams.get('callerUser');
+    const callerUser = location.state.callerUser;
+    // const callerUserSignal = searchParams.get('callerUserSignal');
+    const callerUserSignal = location.state.callerUserSignal;
+    const username = location.state.username;
+    // console.log('isAnswerCall',typeof isAnswerCall,isAnswerCall);
+    // const socket = io('http://localhost:8000', {
+    //     auth: {
+    //         Authorization: "Bearer " + sessionStorage.getItem('token')
+    //     }
+    // });
+
+    async function setStreamFunc() {
+        const stream1 = await navigator.mediaDevices.getUserMedia({ video: videoParam, audio: true })
+        setStream(stream1);
+        console.log('stream1', stream1);
+        if (myVideo.current) {
+            myVideo.current.srcObject = stream1
+            if (isAnswerCall) {
+                console.log('answerCall');
+                answerCall(stream1);
+            } else {
+                callUser(recipientParam, stream1);
+            }
+        }
+    };
 
     useEffect(() => {
-        if (isAnswerCall) {
-            answerCall();
-        }
-        else {
-            callUser(recipientParam)
-        }
-        navigator.mediaDevices.getUserMedia({ video: videoParam, audio: true }).then((stream) => {
-            setStream(stream)
-            if (myVideo.current)
-                myVideo.current.srcObject = stream
-        })
-
-        socket.on("me", (id) => {
-            setMe(id)
-        })
-
-        // socket.on("callUser", (data) => {
-        //     setReceivingCall(true)
-        //     setCaller(data.from)
-        //     setName(data.name)
-        //     setCallerSignal(data.signal)
-        // })
+        setStreamFunc();
     }, [])
 
-    const callUser = (id) => {
+    // useEffect(() => {
+    //     navigator.mediaDevices.getUserMedia({ video: videoParam, audio: true }).then((stream) => {
+    //         setStream(stream)
+    //         if (myVideo.current) {
+    //             myVideo.current.srcObject = stream
+
+    //             myVideo.current.addEventListener('loadedmetadata', () => {
+    //                 // This code will be executed after the stream is set on the video element
+    //                 console.log('myVideo', myVideo.current.srcObject);
+
+    //                 if (isAnswerCall) {
+    //                     console.log('answerCall');
+    //                     answerCall();
+    //                 } else {
+    //                     callUser(recipientParam);
+    //                 }
+    //             });
+    //             // if (isAnswerCall) {
+    //             //     console.log('answerCall');
+    //             //     answerCall();
+    //             // }
+    //             // else {
+    //             //     callUser(recipientParam)
+    //             // }
+    //             console.log('myVideo', myVideo.current.srcObject);
+    //         }
+    //     })
+    //     // if (isAnswerCall) {
+    //     //     console.log('answerCall');
+    //     //     answerCall();
+    //     // }
+    //     // else {
+    //     //     callUser(recipientParam)
+    //     // }
+    //     console.log(stream, 'myStream');
+
+    //     // socket.on("me", (id) => {
+    //     //     console.log('me', id);
+    //     //     setMe(id)
+    //     // })
+
+    //     // socket.on("callUser", (data) => {
+    //     //     console.log('awel call user');
+    //     //     console.log('callUser', data.name);
+    //     //     setReceivingCall(true)
+    //     //     setCaller(data.from)
+    //     //     setName(data.name)
+    //     //     setCallerSignal(data.signal)
+    //     // })
+    // }, [])
+
+    const callUser = (id, stream) => {
+        console.log('tany call user');
+        console.log('stream', stream);
         const peer = new Peer({
             initiator: true,
             trickle: false,
             stream: stream
         });
 
+        console.log('callUser', peer);
+
         peer.on("signal", (data) => {
+            console.log('callUser', data);
+            console.log('me', me);
+            console.log('name', name);
             // Include the user ID in the call information
             socket.emit("callUser", {
                 userToCall: id,
                 signalData: data,
                 from: me,
-                name: name,
+                name: username,
             });
         });
 
         peer.on("stream", (stream) => {
+            console.log('stream', stream);
             userVideo.current.srcObject = stream;
+            console.log('userVideo', userVideo.current.srcObject);
         });
 
         peer.on('close', () => {
             setCallEnded(true);
             connectionRef.current.destroy();
         });
-
+        console.log('callUser', me);
         socket.on("callAccepted", (signal) => {
+            console.log('callAccepted', 'el signal', signal);
             setCallAccepted(true);
             peer.signal(signal);
         });
@@ -106,32 +179,45 @@ function Room() {
         connectionRef.current = peer;
     };
 
-    const answerCall = () => {
+    const answerCall = (stream) => {
         setCallAccepted(true)
+        console.log('answerCall', caller);
+        console.log('answerCall', callerUserSignal);
+        console.log('answerCall', stream);
         const peer = new Peer({
             initiator: false,
             trickle: false,
             stream: stream
         })
+
+        console.log('callUser', peer);
+
         peer.on("signal", (data) => {
-            socket.emit("answerCall", { signal: data, to: caller })
+            console.log('answerCall', data);
+            console.log('answerCall', callerUser);
+            socket.emit("answerCall", { signal: data, to: callerUser });
         })
         peer.on("stream", (stream) => {
+            console.log('stream', stream);
             userVideo.current.srcObject = stream
+            console.log('userVideo', userVideo.current.srcObject);
         })
 
         peer.on('close', () => {
             setCallEnded(true)
         })
 
-        peer.signal(callerSignal)
+        peer.signal(callerUserSignal)
         connectionRef.current = peer
     }
 
     const leaveCall = () => {
         setCallEnded(true)
-        if (connectionRef.current)
+        if (connectionRef.current){
             connectionRef.current.destroy()
+            socket.emit("endCall", { to: callerUser })
+            navigate('/chat');
+        }
     }
 
     const closeVideo = () => {
@@ -159,14 +245,15 @@ function Room() {
     return (
         <>
             <div id="videos">
-                <div className="video-player">
+                <div className="absolute">
                     {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
                 </div>
                 <div className="video-player" id="user-2">
-                    {callAccepted && !callEnded ?
-
-                        <video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
-                        null}
+                    {
+                        callAccepted && !callEnded ?
+                            <video playsInline ref={userVideo} autoPlay style={{ width: "100%",height:"100%" }} /> :
+                            null
+                    }
                 </div>
             </div>
 
