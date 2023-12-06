82a806a1ec4e7470366c50b1cc8191324e38ee04
AbdullahElNahas <105518820+AbdullahElNahas@users.noreply.github.com>
2023-12-06 21:31:33 +0200

    viewMyPatientPrescAdded

--- frontend/src/components/viewMyPatientPresc.js
+++ frontend/src/components/viewMyPatientPresc.js
@@ -1,4 +1,11 @@
-function viewMyPatientPresc(){
+import "../Styles/LoginForm.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import profileImage from "../Assets/profile.png";
+function ViewMyPatientPresc(){
+    const params = new URLSearchParams(window.location.search);
+    const userId = params.get('Id');
+    const [myPatient, setMyPatient] = useState({});
 
 }
 export default ViewMyPatientPresc;
