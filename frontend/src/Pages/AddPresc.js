3ac7dce89925dcc432b18bbc458df5e3991930d0
AbdullahElNahas <105518820+AbdullahElNahas@users.noreply.github.com>
2023-12-06 23:07:20 +0200

    addPresc_Updated

--- frontend/src/Pages/AddPresc.js
+++ frontend/src/Pages/AddPresc.js
@@ -1,5 +1,7 @@
 import "../Styles/addPresc.css"
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import Meds from "../components/Meds";
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -7,7 +9,6 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import SplitButton from 'react-bootstrap/SplitButton';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
 const AddPresc = () => {
   const [allMedicines, setAllMedicines] = useState([]);
@@ -32,53 +33,6 @@ const AddPresc = () => {
 
     getMedicines();
   }, []);
-
-//   const handleSearch = async () => {
-//     const inputValue = document.getElementById('searchInput').value;
-//     try {
-//       const response = await axios.get(`http://localhost:8000/Doctor/getMedicine/${inputValue}`, { headers: { Authorization: 'Bearer ' + sessionStorage.getItem("token") } });
-
-//       if (response.status === 200) {
-//         const result = response.data.Result;
-
-//         if (result && result.Quantity > 0) {
-//           setAllMedicines([response.data.Result]);
-//         } else if (result && result.Quantity === 0) {
-//           setErrorMessage(`The medicine is out of stock. Here are some alternatives.`);
-
-//           // Set a timer to clear the error message after 2 minutes (adjust as needed)
-//           setTimeout(() => {
-//             setErrorMessage('');
-//           }, 4000); 
-
-//           const alternativesResponse = await axios.get(`http://localhost:8000/Doctor/findAlternatives/${inputValue}`, { headers: { Authorization: 'Bearer ' + sessionStorage.getItem("token") } });
-
-//           if (alternativesResponse.status === 200) {
-//             setAllMedicines(alternativesResponse.data.Alternatives);
-//           } else {
-//             console.error('Failed to find alternatives. Unexpected response:', alternativesResponse);
-//           }
-//         }
-//       } else {
-//         console.error('Failed to search medicines. Unexpected response:', response);
-//       }
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
-
-//   const handleMedicalUseFilter = async (medicalUse) => {
-//     try {
-//       const response = await axios.get(`http://localhost:8000/Doctor/filterMedical/${medicalUse}`, { headers: { Authorization: 'Bearer ' + sessionStorage.getItem("token") } });
-//       if (response.status === 200) {
-//         setAllMedicines(response.data.Result);
-//       } else {
-//         console.error('Failed to filter medicines. Unexpected response:', response);
-//       }
-//     } catch (error) {
-//       console.error('Error:', error);
-//     }
-//   };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -106,28 +60,6 @@ const AddPresc = () => {
 
   return (
     <div>
-      {/* <InputGroup className="mb-3">
-        <SplitButton
-          variant="outline-secondary"
-          title="Search"
-          id="segmented-button-dropdown-1"
-          onClick={handleSearch}
-        >
-          <Dropdown.Header>Filter</Dropdown.Header>
-          {medicalUses.map((use, index) => (
-            <Dropdown.Item key={index} onClick={() => handleMedicalUseFilter(use)}>
-              {use}
-            </Dropdown.Item>
-          ))}
-          <Dropdown.Divider />
-        </SplitButton>
-        <Form.Control
-          id="searchInput"
-          type="search"
-          placeholder="Search"
-          aria-label="Text input for search"
-        /> 
-      </InputGroup>*/}
       {errorMessage && (
         <div className="alert alert-danger" role="alert">
           {errorMessage}
