0f1b5e2f787fc7d659463a682f99c459d9e3abe9
Youssef Eid <yeid@sumerge.com>
2023-12-16 19:24:20 +0200

    Added empty list handling in Doc Requests..

--- frontend/src/components/CreateAdmin.js
+++ frontend/src/components/CreateAdmin.js
@@ -13,13 +13,13 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import Nav from "../components/Admin-NavBar.js";
 
-
 const defaultTheme = createTheme();
 
 const CreateAdmin = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [response, setResponse] = useState('');
+  const [messageType, setMessageType] = useState('');
 
   const enableCreateButton = () => {
     return username.trim() !== '' && password.trim() !== '';
@@ -41,77 +41,85 @@ const CreateAdmin = () => {
       const data = response.data;
 
       if (data.success) {
-        setResponse(`
-          Username: ${data.savedAdmin.Username}
-          Password: ${data.savedAdmin.Password}
-        `);
+        setResponse("Admin "+data.Result.Username+" created successfully!");
+        setMessageType('success');
+        // Clear the fields after success
+        setUsername('');
+        setPassword('');
       } else {
         setResponse(`Error: ${data.message}`);
+        setMessageType('error');
       }
     } catch (error) {
-      console.error('Error:', error);
-      setResponse(error.message);
+      console.error('Error:', error.response.data.message);
+      setResponse('Error:', error.response.data.message);
+      setMessageType('error');
     }
   };
 
   return (
     <div>
-      <Nav/>
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Admin Creation Form
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              autoComplete="new-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={!enableCreateButton()}
-            >
-              Create Admin
-            </Button>
+      <Nav />
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Admin Creation Form
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                autoComplete="username"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="new-password"
+              />
+              {messageType && (
+                <Typography variant="body1" sx={{ color: messageType === 'success' ? 'green' : 'red' }}>
+                  {response}
+                </Typography>
+              )}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={!enableCreateButton()}
+              >
+                Create Admin
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 };
